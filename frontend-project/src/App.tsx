922100be208a2edddf98f2c87288935c6a663a63
zengqignhe <xioahe0323@126.com>
2022-07-07 22:55:31 +0800

    登录框

--- frontend-project/src/App.tsx
+++ frontend-project/src/App.tsx
@@ -1,11 +1,58 @@
-import React, { FC } from 'react';
-import { Button } from 'antd';
 import './login.css';
+import { Button, Form, Input } from 'antd';
 
-const App: FC = () => (
-  <div className="App">
-    <Button type="primary">Button</Button>
-  </div>
-);
+const App = () => {
+  const onFinish = (values:any) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <div className="login-page">
+    <Form
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          登录
+        </Button>
+      </Form.Item>
+    </Form>
+    </div>
+  );
+};
 
 export default App;
